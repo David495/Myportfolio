8ebc988c3963ea1993339898f9795dd7f52e9b2f
david <davidnwaogu961@gmail.com>
2025-06-27 16:27:08 +0100

    My unresponsive portfolio

---
+++ services.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamMenu = document.querySelector('.hamMenu');
+    const offScreenMenu = document.querySelector('.offScreenMenu');
+
+    hamMenu.addEventListener('click', () => {
+        hamMenu.classList.toggle('active');
+        offScreenMenu.classList.toggle('active');
+    });
+});
